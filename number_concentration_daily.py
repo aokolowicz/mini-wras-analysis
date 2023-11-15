7081a72b99a35d1d6c37e333dfc29d7de0a6e479
Adrian <130671043+aokolowicz@users.noreply.github.com>
2023-11-15 15:25:58 +0100

    Added code to analyze and visualize number concentration data

---
+++ number_concentration_daily.py
@@ -0,0 +1,112 @@
+import math
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from matplotlib import ticker
+from helpers import (
+    directory_tree,
+    get_path,
+    list_files,
+    tell_parent,
+    y_formatter_function,
+    mm,
+    label_font,
+    tick_font,
+)
+
+# File handling (indexes in col=0, conversion needed)
+dir_tree = directory_tree()
+files = list_files(dir_tree, "one", ".dat")
+df = pd.read_table(get_path(dir_tree, files[0]), skiprows=10, index_col=0)
+nano = df.iloc[:, 1:9]
+nano.insert(
+    loc=0,
+    column="total nano",
+    value=[sum(nano.iloc[i]) for i in range(nano.shape[0])],
+)
+df.index = pd.to_datetime(df.index, dayfirst=True)
+
+# Calculate average values
+avg_conc = df["total counts"].mean()
+avg_nano_conc = nano["total nano"].mean()
+
+# Plotting
+plt.figure(figsize=(160 * mm, 120 * mm), dpi=300)
+
+# Plot total particles concentration and average
+plt.plot(
+    df.index, df["total counts"], "k.:", linewidth=1, label="Total particles"
+)
+plt.plot(
+    [df.index[0], df.index[-1]],
+    [avg_conc, avg_conc],
+    "k-",
+    linewidth=0.5,
+    label="Total particles (mean)",
+)
+
+# Plot nanoparticles concentration and average
+plt.plot(
+    df.index, nano["total nano"], "r.:", linewidth=1, label="Nanoparticles"
+)
+plt.plot(
+    [df.index[0], df.index[-1]],
+    [avg_nano_conc, avg_nano_conc],
+    "r-",
+    linewidth=0.5,
+    label="Nanoparticles (mean)",
+)
+
+plt.legend(loc="best", fontsize=6)
+
+# X-axis
+xformatter = mdates.DateFormatter("%H:%M")
+xlower = df.index[0].floor("H")
+xupper = df.index[-1].ceil("H")
+plt.gca().xaxis.set_major_formatter(xformatter)
+plt.xticks(**tick_font)
+plt.xlabel("Time [hh:mm]", **label_font)
+plt.xlim(xlower, xupper)
+
+# Y-axis
+yformatter = ticker.FuncFormatter(y_formatter_function)
+ylocator = (
+    ticker.LogLocator(base=10)
+    if df["total counts"].max() > 4e4
+    else ticker.LinearLocator()
+)
+plt.gca().yaxis.set_major_formatter(yformatter)
+plt.gca().yaxis.set_major_locator(ylocator)
+plt.ylabel("Number concentration [$\mathregular{1/cm^3}$]", **label_font)
+plt.yticks(**tick_font)
+
+# Calculate y-axis limits
+ylower = 100 if df["total counts"].max() > 4e4 else 0
+coeff = 1000 if df["total counts"].max() < 4e4 else 1e6
+yupper = math.ceil(df["total counts"].max() / coeff) * coeff
+plt.ylim(ylower, yupper)
+
+# Show average values on graph
+for avg, color in zip([avg_conc, avg_nano_conc], ["k", "r"]):
+    plt.annotate(
+        f"{avg:,.0f}".replace(",", " "),
+        xy=(xupper, avg),
+        xytext=(-6 * len(str(round(avg))), 4),
+        fontsize=6,
+        textcoords="offset points",
+        bbox=dict(boxstyle="round", fc="w", ec=color, alpha=0.8),
+    )
+
+# Save figure if requested
+save_figure = input("Save figure? (Y/n)\n")
+if save_figure.lower() != "n":
+    fig_name = f'Total Number and Nano ({files[0][:files[0].rfind("-")]})'
+    fig_path = (
+        get_path(dir_tree, tell_parent(get_path(dir_tree, files[0])))
+        + f"\\{fig_name}.png"
+    )
+    plt.savefig(fig_path, bbox_inches="tight")
+    print(f"Figure saved as {fig_name}.png")
+else:
+    print("Figure not saved.")
