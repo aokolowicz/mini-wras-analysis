aed3dc42c6946a12411df22cb0f0b8c5ffe20754
Adrian <130671043+aokolowicz@users.noreply.github.com>
2023-11-16 16:01:59 +0100

    Added num_to_mass function to convert number to mass concentration. Added save_figure function. Added path variable. Small improvements in y_formatter_function

--- helpers.py
+++ helpers.py
@@ -1,16 +1,25 @@
+import math
+import matplotlib.pyplot as plt
 import os
+import pandas as pd
+
+# Define the path to the data
+path = r'C:\Users\Adrian\Desktop\repos\mini-wras-analysis\sample-data'
 
 # Define constants
 mm = 1 / 25.4  # Conversion factor from inches to mm
 ro = 1680  # kg/m3
-conv_fact = 1.48  # Comparison between mass from MINI-WRAS and calculations
+corr_fact = 1.48  # Correct the calculated mass concentration
+
+# Define font parameters
+title_font = {'fontname': 'Verdana', 'fontsize': 14, 'weight': 'bold'}
 label_font = {'fontname': 'Verdana', 'fontsize': 8, 'weight': 'bold'}
 tick_font = {'fontname': 'Verdana', 'fontsize': 8}
 
 
 def directory_tree(path=os.getcwd()):
     """
-    Generates a nested dictionary representing the directory
+    Generate a nested dictionary representing the directory
     tree.
     """
     tree = {}
@@ -33,7 +42,7 @@ def directory_tree(path=os.getcwd()):
 
 def get_path(tree, tree_item, prefix=os.getcwd()):
     """
-    Recursively searches for a target file in a nested directory
+    Recursively searche for a target file in a nested directory
     tree.
     """
     for item, subtree in tree.items():
@@ -78,9 +87,49 @@ def list_files(tree, keyword, file_extension):
     return files
 
 
+def num_to_mass(dataframe, ro, conv_fact=1):
+    """
+    Convert number concentrations in 1/cm^3 to mass concentrations in mg/m^3
+    using provided density.
+    """
+    # Create an empty DataFrame with the same index as the input DataFrame
+    mass_df = pd.DataFrame(index=dataframe.index)
+    
+    # Loop through columns in the input DataFrame
+    for col in dataframe:
+        # Try to convert column name col (str) to diameter d (int).
+        # Omit col='total counts'.
+        try:
+            d = int(col) * 1e-9  # Convert nanometers to meters
+        except ValueError:
+            continue
+        
+        # Calculate the volume of a particle using the diameter d
+        V = 4/3 * math.pi * (d / 2) ** 3  # m^3
+        
+        # Calculate the total volume of particles of a particular size
+        # as pandas.Series and convert to m^3
+        V_total = V * dataframe[col] * 1e6  # m^3
+        
+        # Calculate the mass of particles using the density ro 
+        # as pandas.Series and convert to mg
+        mass = V_total * ro * 1e6  # mg
+        
+        # Add the calculated mass pandas.Series to the mass DataFrame
+        mass_df[col] = mass * conv_fact  # Apply conversion factor
+    
+    # Calculate the total mass for each row and insert the 'total mass' column
+    mass_df.insert(loc=0,
+                   column='total mass',
+                   value=[mass_df.iloc[i].sum() for i in range(mass_df.shape[0])])
+    
+    # Return the DataFrame containing mass concentrations
+    return mass_df
+
+
 def print_directory_tree(tree, indent=0):
     """
-    Prints a nested dictionary representing the directory tree
+    Print a nested dictionary representing the directory tree
     with proper indents.
     """
     for item, subtree in tree.items():
@@ -89,6 +138,18 @@ def print_directory_tree(tree, indent=0):
         else:
             print('  ' * indent + '+ ' + item)
             print_directory_tree(subtree, indent + 1)
+
+
+def save_figure(fig_name, fig_path):
+    """
+    Prompt user to save the current figure.
+    """
+    save_figure = input("Save figure? (Y/n)\n")
+    if save_figure.lower() != "n":       
+        plt.savefig(fig_path)
+        print(f"Figure saved as {fig_name}.png")
+    else:
+        print("Figure not saved.")
 
 
 def tell_parent(item_path):
@@ -111,20 +172,18 @@ def tell_parent(item_path):
 
 def y_formatter_function(x, pos):
     """
-    FuncFormatter: Use a user-defined function for formatting. The function
-    should take in two inputs (a tick value x and a position pos), and return
-    a string containing the corresponding tick label.
+    Custom formatter function for y-axis ticks.
     """
     if x == 0:
-        return '{:,}'.format(int(x)).replace(',', ' ')
+        return '{:,}'.format(int(x))
     elif x < 0.01:
-        return '{:,.3f}'.format(float(x)).replace(',', ' ').replace('.', ',')
+        return '{:,.3f}'.format(float(x))
     elif x < 0.1:
-        return '{:,.2f}'.format(float(x)).replace(',', ' ').replace('.', ',')
+        return '{:,.2f}'.format(float(x))
     elif x < 1:
-        return '{:,.1f}'.format(float(x)).replace(',', ' ').replace('.', ',')
+        return '{:,.1f}'.format(float(x))
     else:
-        return '{:,}'.format(int(x)).replace(',', ' ')
+        return '{:,}'.format(int(x))
 
 
 if __name__ == '__main__':
