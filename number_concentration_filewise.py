7ed21e02d06db77ee8760a56a1d4caf96d6f13ff
Adrian <130671043+aokolowicz@users.noreply.github.com>
2024-04-03 15:52:46 +0200

    Refactor code into modular functions for improved readability and maintainability

--- number_concentration_filewise.py
+++ number_concentration_filewise.py
@@ -12,17 +12,48 @@ from helpers import (
     save_figure,
     tell_parent,
     y_formatter_function,
+    print_directory_tree,
     mm,
     label_font,
     tick_font,
+    path,
 )
 
-# File handling (indexes in col=0, conversion needed)
-# path can be specified - add it in get_path() function as well!
-dir_tree = directory_tree()
-files = list_files(dir_tree, 'day', 'C.dat')    # C.dat for number concentration
-for file in files:
-    df = pd.read_table(get_path(dir_tree, file), skiprows=10, index_col=0)
+
+def main():
+    # File handling, indexes in col=0, conversion needed in process_file()
+    dir_tree = directory_tree(path)
+    files = list_files(dir_tree, 'location', 'C.dat')  # C.dat for number concentration
+    for file in files:
+        df, nano = process_file(dir_tree, file)
+
+        # Calculate average values
+        avg_conc = df['total counts'].mean()
+        avg_nano_conc = nano['total nano'].mean()
+
+        plot_data(df, nano, avg_conc, avg_nano_conc)
+        _, xupper, _, _ = set_axes(df)
+        annotate_averages(avg_conc, avg_nano_conc, xupper)
+
+        parent = tell_parent(get_path(dir_tree, file, path))
+        # print_directory_tree(dir_tree)
+        print(f"{parent}: ", get_path(dir_tree, parent, path), "\n\n")
+
+        # Save figure if requested
+        fig_name = generate_fig_name(file)
+        fig_path = os.path.join(
+            get_path(
+                dir_tree, tell_parent(get_path(dir_tree, file, path)), path
+            ),
+            f'{fig_name}.png',
+        )
+        save_figure(fig_name, fig_path)
+
+
+def process_file(tree, file):
+    """Read data to the pandas.DataFrame and prepare for analysis."""
+
+    df = pd.read_table(get_path(tree, file, path), skiprows=10, index_col=0)
 
     # Nanoparticles are in the first 8 columns (from 10 to 100 nm),
     # without column 0 where the total counts for all particles are
@@ -30,15 +61,18 @@ for file in files:
     nano.insert(
         loc=0,
         column='total nano',
-        value=[sum(nano.iloc[i]) for i in range(nano.shape[0])],
+        value=nano.sum(axis=1),
     )
+
+    # Convert index to datetime
     df.index = pd.to_datetime(df.index, dayfirst=True)
 
-    # Calculate average values
-    avg_conc = df['total counts'].mean()
-    avg_nano_conc = nano['total nano'].mean()
+    return df, nano
 
-    # Plotting
+
+def plot_data(df, nano, avg_conc, avg_nano_conc):
+    """Generate plot."""
+
     plt.figure(figsize=(160 * mm, 120 * mm), dpi=300)
 
     # Plot total particles concentration and average
@@ -70,6 +104,10 @@ for file in files:
     )
 
     plt.legend(loc='best', fontsize=6)
+
+
+def set_axes(df):
+    """Set properties of the axes."""
 
     # X-axis
     xformatter = mdates.DateFormatter('%H:%M')
@@ -103,7 +141,12 @@ for file in files:
     yupper = math.ceil(df['total counts'].max() / coeff) * coeff
     plt.ylim(ylower, yupper)
 
-    # Show average values on graph
+    return xlower, xupper, ylower, yupper
+
+
+def annotate_averages(avg_conc, avg_nano_conc, xupper):
+    """Show average values on graph."""
+
     for avg, color in zip([avg_conc, avg_nano_conc], ['k', 'r']):
         plt.annotate(
             f'{avg:,.0f}'.replace(',', ' '),
@@ -114,10 +157,10 @@ for file in files:
             bbox=dict(boxstyle='round', fc='w', ec=color, alpha=0.8),
         )
 
-    # Save figure if requested
-    fig_name = f"Total Number and Nano ({file[:file.rfind('-')]})"
-    fig_path = os.path.join(
-        get_path(dir_tree, tell_parent(get_path(dir_tree, file))),
-        f'{fig_name}.png',
-    )
-    save_figure(fig_name, fig_path)
+
+def generate_fig_name(file):
+    return f"Total Number and Nano ({file[:file.rfind('-')]})"
+
+
+if __name__ == '__main__':
+    main()
